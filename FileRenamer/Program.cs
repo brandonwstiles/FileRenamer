f7f482cc22f810fa4aabec54821f0058c071c7d0
Brandon <brandonwstiles@gmail.com>
2018-07-23 12:35:50 -0500

    Add project files.

---
+++ FileRenamer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileRenamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
